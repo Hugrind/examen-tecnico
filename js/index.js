468b2fb1e5d013277f2387702b7d97fa498c2a44
Hugo Duran <vdurans@ucenfotec.ac.cr>
2021-05-01 18:43:31 -0600

    dsasa
    saa

---
+++ js/index.js
@@ -0,0 +1,18 @@
+'use strict';
+const txtFechaTarea = document.querySelector('#txt-fecha-tarea');
+const txtNombreTarea = document.querySelector('#txt-nombre-tarea');
+const txtDescripcionTarea = document.querySelector('#txt-descripcion-tarea');
+const sltCategoria = document.querySelector('#slt-categoria');
+const sltEncargado = document.querySelector('#slt-encargado');
+const btnAgregar = document.querySelector('#btn-agregar');
+
+
+
+
+
+
+
+
+
+
+btnAgregar.addEventListener('click', obtener_datos);
